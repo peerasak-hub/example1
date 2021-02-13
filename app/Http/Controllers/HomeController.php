ce9571649414197346e2958f96924f2b18de6df5
peerasak-hub <78993119+peerasak-hub@users.noreply.github.com>
2021-02-13 14:28:16 +0700

    เพิ่มmaster

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
     {
         return view('contact');
     }
-    public function hello($name,$age)
+    public function hello($name, $age)
     {
         $data = [
             'name' => $name,
